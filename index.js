9422928c29f07495725e12875f293f9942d7e640
Rallge <geraldinelopz@gmail.com>
2021-07-14 02:23:07 +0200

    Si no se agrega el parametro next, entonces no va al siguiente paso y se queda en paso 1

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ app.listen(3010, (err) => {
   }
   console.log("Servidor escuchando en el puerto 3010");
 });
+
+// Creando cadena de middleware
+// Recoge la request y hace lo que quiere con ella xD
+
+// Por convencion : request = req , respuesta = res , siguiente = next
+
+app.use(() =>{
+  console.log("Paso 1");
+});
+
+app.use(() => {
+  console.log("Paso 2");
+});
+
+app.use(() => {
+  console.log("Paso 3");
+});
