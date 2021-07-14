a77d2201552477c7721a8d758b42380c75b2f990
Rallge <geraldinelopz@gmail.com>
2021-07-14 02:25:18 +0200

    SE agrego next para que pase por todos los puntos

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ app.listen(3010, (err) => {
 
 // Por convencion : request = req , respuesta = res , siguiente = next
 
-app.use(() =>{
+app.use((req,res,next) =>{
   console.log("Paso 1");
+  next();
 });
 
-app.use(() => {
+app.use((req,res,next) => {
   console.log("Paso 2");
+  next();
 });
 
-app.use(() => {
+app.use((req,res,next) => {
   console.log("Paso 3");
+  next();
 });
